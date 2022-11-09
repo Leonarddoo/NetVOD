5cccdadd3b33b20ba5aed36f4f6185f5c635146d
Vivien STEMMELEN <vivien.stemmelen2@etu.univ-lorraine.fr>
2022-11-09 15:10:15 +0100

    rectification de l'ajout de série aux préférences de l'utilisateur

--- src/classes/action/AccueilAction.php
+++ src/classes/action/AccueilAction.php
@@ -5,6 +5,7 @@ namespace iutnc\netvod\action;
 use iutnc\netvod\action\Action;
 use iutnc\netvod\auth\Auth;
 use iutnc\netvod\db\ConnectionFactory;
+use iutnc\netvod\User;
 
 class AccueilAction extends Action
 {
@@ -15,12 +16,18 @@ class AccueilAction extends Action
          * @param string $output
          * @return string
          */
-        $output = '';
+        $output = 'Préférences :';
         switch ($this->http_method) {
             case 'GET':
                 $PDO = ConnectionFactory::makeConnection();
-                $statement = $PDO->prepare('SELECT * FROM userPreference');
+                $statement = $PDO->prepare('SELECT * FROM userPreference INNER JOIN serie ON id_serie=serie.id WHERE id_user = ?');
+
+                $user = unserialize($_SESSION['user']);
+                $id_user = $user->id;
+
+                $statement->bindParam(1, $id_user);
                 $statement->execute();
+
                 $output .= '<ul>';
                 while ($data=$statement->fetch()) {
                     $output .= "<li>{$data['titre']}<img src='{$data['img']}' alt='Une image correspondant à la série'></li>";
