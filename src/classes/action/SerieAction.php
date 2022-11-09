eeb6808dad8fa77d3ae6bb19edc919092becc971
Hugo Demange <hugo.demange8@etu.univ-lorraine.fr>
2022-11-09 13:46:22 +0100

    ajout gestion des préférences dans les séries

--- src/classes/action/SerieAction.php
+++ src/classes/action/SerieAction.php
@@ -2,6 +2,7 @@
 
 namespace iutnc\netvod\action;
 
+use iutnc\netvod\auth\Auth;
 use iutnc\netvod\db\ConnectionFactory;
 use iutnc\netvod\Utils;
 
@@ -15,15 +16,15 @@ class SerieAction extends Action
             $PDO = ConnectionFactory::makeConnection();
 
             $serie_statement = $PDO->prepare('SELECT serie.titre title, serie.descriptif sum, serie.annee year, serie.date_ajout date, count(episode.id) nb_ep FROM serie INNER JOIN episode on serie.id = episode.serie_id WHERE serie.id = ? GROUP BY serie.id, serie.titre');
-            $serie_statement->bindParam(1,$id);
+            $serie_statement->bindParam(1, $id);
             $serie_statement->execute();
 
             $genre_statement = $PDO->prepare('SELECT libelleGenre FROM genre INNER JOIN estGenreDe on genre.id_genre = estGenreDe.id_genre WHERE id_serie = ?');
-            $genre_statement->bindParam(1,$id);
+            $genre_statement->bindParam(1, $id);
             $genre_statement->execute();
 
             $public_statement = $PDO->prepare('SELECT libelle FROM public INNER JOIN estPublicDe on public.id_public = estPublicDe.id_public WHERE id_serie = ?');
-            $public_statement->bindParam(1,$id);
+            $public_statement->bindParam(1, $id);
             $public_statement->execute();
 
             if ($serie = $serie_statement->fetch()) { // si la serie dont l'id est $id existe
@@ -41,12 +42,17 @@ class SerieAction extends Action
                 $output .= "<div>Année de sortie : {$serie['year']}</div>";
                 $output .= "<div>Date d'ajout : {$serie['date']}</div>";
 
+                if ($connected = Auth::connected()) {
+//                    $output .= "<form method='post'><button type='submit' name='like' value='true'>Ajouter à mes préférences</button></form>";
+                    $output .= Utils::linked_button('Ajouter à mes préférences', 'true', 'post', 'like');
+                }
+
                 $ep_statement = $PDO->prepare('SELECT e.titre title, numero, duree, resume, e.img FROM serie INNER JOIN episode e on serie.id = e.serie_id WHERE serie.id = ? ORDER BY numero');
-                $ep_statement->bindParam(1,$id);
+                $ep_statement->bindParam(1, $id);
                 $ep_statement->execute();
 
                 $output .= '<ul>';
-                while ($ep=$ep_statement->fetch()) {
+                while ($ep = $ep_statement->fetch()) {
                     $output .= '<li>';
 
                     $output .= "Episode n° {$ep['numero']} : {$ep['title']}, durée : {$ep['duree']}";
@@ -56,6 +62,21 @@ class SerieAction extends Action
                     $output .= '</li>';
                 }
                 $output .= '</ul>';
+
+                if ($this->http_method === 'POST' and $connected) {
+                    if (isset($_POST['like'])) {
+                        if ($_POST['like'] === 'true') {
+                            $like_statement = $PDO->prepare('INSERT INTO userPreference VALUES (?, ?)');
+
+                            $id_user = unserialize($_SESSION['USER']);
+
+                            $like_statement->bindParam(1, $id_user);
+                            $like_statement->bindParam(1, $id);
+
+                            $like_statement->execute();
+                        }
+                    }
+                }
             }
         }
         return $output;
