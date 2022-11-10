ff48844850fca3ad6134b3a03ec334f0a1720c76
Vivien STEMMELEN <vivien.stemmelen2@etu.univ-lorraine.fr>
2022-11-10 10:30:00 +0100

    modification EpisodeAction

--- src/classes/action/EpisodeAction.php
+++ src/classes/action/EpisodeAction.php
@@ -2,10 +2,8 @@
 
 namespace iutnc\netvod\action;
 
-use iutnc\netvod\auth\Auth;
 use iutnc\netvod\db\ConnectionFactory;
 use iutnc\netvod\User;
-use iutnc\netvod\Utils;
 use PDOException;
 
 class EpisodeAction extends Action
@@ -15,55 +13,34 @@ class EpisodeAction extends Action
         $output = '';
         if ($id = filter_var($_GET['id'], FILTER_VALIDATE_INT)) {
 
-                $PDO = ConnectionFactory::makeConnection();
+            $PDO = ConnectionFactory::makeConnection();
 
-                $episode_statement= $PDO->prepare('SELECT id i,numero num ,titre title,resume res,duree dur,file fil,serie_id idse,img FROM episode WHERE episode.id = ?');
-                $episode_statement-> bindParam(1,$id);
-                $episode_statement->execute();
+            $episode_statement = $PDO->prepare('SELECT id i,numero num ,titre title,resume res,duree dur,file fil,serie_id idse,img FROM episode WHERE episode.id = ?');
+            $episode_statement->bindParam(1, $id);
+            $episode_statement->execute();
 
+            if ($episode = $episode_statement->fetch()) {
+                $output .= "<h1><div> Episode n° {$episode['num']}</div></h1>";
+                $output .= "<h2>{$episode['title']}</h2>";
 
-
-
-                if($episode = $episode_statement->fetch()){
-                    $output .="<h1><div> Episode n° {$episode['num']}</div></h1>";
-                    $output .= "<h2>{$episode['title']}</h2>";
-
-                    $output .= "<div></div><video controls src='video/{$episode['fil']}'></video></div>";
+                $output .= "<div></div><video controls src='video/{$episode['fil']}'></video></div>";
 
 
-                    $output .="<img src='{$episode['img']}' alt=\"Une image correspond à l'episode\">";
+                $output .= "<img src='{$episode['img']}' alt=\"Une image correspond à l'episode\">";
 
-                    $output .= "<div> Résumé : <p>{$episode['res']}</p></div>";
-                    $output .= "<div> Durée: <p>{$episode['dur']} min</p></div>";
+                $output .= "<div> Résumé : <p>{$episode['res']}</p></div>";
+                $output .= "<div> Durée: <p>{$episode['dur']} min</p></div>";
 
-                    $id_user = User::sessionUser()->id;
-                    $serie = $episode['idse'];
+                $id_user = User::sessionUser()->id;
+                $serie = $episode['idse'];
 
-                    $watch_statement = $PDO->prepare('INSERT INTO userWatch VALUES (?, ?)');
+                $watch_statement = $PDO->prepare('INSERT INTO userWatch VALUES (:user, :serie, :episode) ON DUPLICATE KEY UPDATE id_ep=:episode');
 
-                    $watch_statement->bindParam(1, $id_user);
-                    $watch_statement->bindParam(2, $serie);
-
-                    try {
-                        $watch_statement->execute();
-                    } catch (PDOException $e) {
-                        if ($e->getCode() !== '23000') {
-                            throw $e;
-                        }
-                    }
-
-
-
-
-
-
-
-                }
-
+                $watch_statement->bindParam('user', $id_user);
+                $watch_statement->bindParam('serie', $serie);
+                $watch_statement->bindParam('episode', $id);
             }
-
-
-
+        }
         return $output;
     }
 }
